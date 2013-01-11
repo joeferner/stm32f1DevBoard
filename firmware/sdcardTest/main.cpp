784543cd318e44e9674220b09e07e88f1a7ffd1c
Joe Ferner <joe@fernsroth.com>
2013-01-11 13:47:14 -0500

    getting sdcard working

--- firmware/sdcardTest/main.cpp
+++ firmware/sdcardTest/main.cpp
@@ -1,33 +1,32 @@
 
 #include <wirish/wirish.h>
+#include <sdcard/Sd2Card.h>
 
-#define PWM_PIN  2
+#define SDCARD_CS  D10
+
+HardwareSPI spi1(1);
+Sd2Card sdcard(spi1);
 
 void setup() {
-  /* Set up the LED to blink  */
+  delay(5000);
+  SerialUSB.println("Begin");
+  sdcard.init(SPI_QUARTER_SPEED, SDCARD_CS);
+  
+  SerialUSB.print("cardSize:");
+  uint32_t cardSize = sdcard.cardSize();
+  SerialUSB.println(cardSize);
+
   pinMode(BOARD_LED_PIN, OUTPUT);
-
-  /* Turn on PWM on pin PWM_PIN */
-  pinMode(PWM_PIN, PWM);
-  pwmWrite(PWM_PIN, 0x8000);
-
-  /* Send a message out USART2  */
-  Serial2.begin(9600);
-  Serial2.println("Hello world!");
-
-  /* Send a message out the usb virtual serial port  */
-  SerialUSB.println("Hello!");
 }
 
 void loop() {
-  toggleLED();
-  SerialUSB.println("Hello!");
+  togglePin(BOARD_LED_PIN);
+  SerialUSB.println("Working");
   delay(1000);
 }
 
 // Force init to be called *first*, i.e. before static object allocation.
 // Otherwise, statically allocated objects that need libmaple may fail.
-
 __attribute__((constructor)) void premain() {
   init();
 }
