8996e7ee392a423d79483537d0eb89fae7a7939d
Joe Ferner <joe@fernsroth.com>
2013-01-13 20:33:18 -0500

    starting usb mass storage work

---
+++ firmware/usbMassStorageTest/main.cpp
@@ -0,0 +1,31 @@
+
+#include <wirish/wirish.h>
+#include <usbMassStorage/usbMassStorage.h>
+
+void setup() {
+  SerialUSB.end();
+  USBMassStorage.begin();
+
+  pinMode(BOARD_LED_PIN, OUTPUT);
+}
+
+void loop() {
+  togglePin(BOARD_LED_PIN);
+  delay(1000);
+}
+
+// Force init to be called *first*, i.e. before static object allocation.
+// Otherwise, statically allocated objects that need libmaple may fail.
+__attribute__((constructor)) void premain() {
+  init();
+}
+
+int main(void) {
+  setup();
+
+  while (true) {
+    loop();
+  }
+
+  return 0;
+}
