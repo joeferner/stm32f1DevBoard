1423ff355c009c2fd2c322b5158317b8f793fb96
Joe Ferner <joe@fernsroth.com>
2013-01-16 15:00:14 -0500

    print block0

--- firmware/sdcardTest/main.cpp
+++ firmware/sdcardTest/main.cpp
@@ -5,17 +5,29 @@
 #define SDCARD_CS  D10
 
 HardwareSPI spi1(1);
-Sd2Card sdcard(spi1);
+Sd2Card sdcard(spi1, SDCARD_CS);
 
 void setup() {
+  uint8_t blockBuffer[512];
+  
   delay(5000);
   SerialUSB.println("Begin");
   spi1.begin(SPI_QUARTER_SPEED, MSBFIRST, 0);
-  sdcard.begin(SPI_QUARTER_SPEED, SDCARD_CS);
-  
+  sdcard.begin();
+
   SerialUSB.print("cardSize:");
-  uint32_t cardSize = sdcard.cardSize();
+  uint32_t cardSize = sdcard.getCardSize();
   SerialUSB.println(cardSize);
+
+  SerialUSB.println("block(0):");
+  sdcard.readBlock(0, blockBuffer);
+  for(int i=0; i<512; ) {
+    for(int col=0; col<16; col++, i++) {
+      SerialUSB.print(blockBuffer[i], 16);
+      SerialUSB.print(" ");
+    }
+    SerialUSB.println();
+  }
 
   pinMode(BOARD_LED_PIN, OUTPUT);
 }
@@ -28,6 +40,7 @@ void loop() {
 
 // Force init to be called *first*, i.e. before static object allocation.
 // Otherwise, statically allocated objects that need libmaple may fail.
+
 __attribute__((constructor)) void premain() {
   init();
 }
