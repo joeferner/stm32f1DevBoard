0ace9558d6e0ed012abab60dd66298da3a2dde2b
Joe Ferner <joe@fernsroth.com>
2013-01-16 16:14:35 -0500

    read first block

--- firmware/usbMassStorageTest/main.cpp
+++ firmware/usbMassStorageTest/main.cpp
@@ -10,11 +10,11 @@ uint32_t MAL_massBlockCount[2];
 uint32_t MAL_massBlockSize[2];
 
 HardwareSPI spi1(SDCARD_SPI);
-Sd2Card sdcard(spi1);
+Sd2Card sdcard(spi1, SDCARD_CS);
 
 void setup() {
   spi1.begin(SPI_QUARTER_SPEED, MSBFIRST, 0);
-  sdcard.begin(SPI_QUARTER_SPEED, SDCARD_CS);
+  sdcard.begin();
   USBMassStorage.begin();
 
   pinMode(BOARD_LED_PIN, OUTPUT);
@@ -65,13 +65,16 @@ extern "C" uint16_t usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset
 }
 
 extern "C" uint16_t usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) {
-  if (lun != 0) {
-    return USB_MASS_MAL_FAIL;
-  }
-  if (sdcard.readBlock(memoryOffset / 512, readbuff)) {
-    return USB_MASS_MAL_SUCCESS;
-  }
-  return USB_MASS_MAL_FAIL;
+  sdcard.readBlock(memoryOffset / 512, readbuff);
+  return USB_MASS_MAL_SUCCESS;
+
+  //  if (lun != 0) {
+  //    return USB_MASS_MAL_FAIL;
+  //  }
+  //  if (sdcard.readBlock(memoryOffset / 512, readbuff)) {
+  //    return USB_MASS_MAL_SUCCESS;
+  //  }
+  //  return USB_MASS_MAL_FAIL;
 }
 
 extern "C" void usb_mass_mal_format() {
