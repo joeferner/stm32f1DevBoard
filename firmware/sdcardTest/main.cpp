5860e3523d06cb97735bf094c388d810b667288a
Joe Ferner <joe@fernsroth.com>
2013-01-16 21:27:28 -0500

    working

--- firmware/sdcardTest/main.cpp
+++ firmware/sdcardTest/main.cpp
@@ -9,7 +9,10 @@ Sd2Card sdcard(spi1, SDCARD_CS);
 
 void setup() {
   uint8_t blockBuffer[512];
-  
+  for (int i = 0; i < 512; i++) {
+    blockBuffer[0];
+  }
+
   delay(5000);
   SerialUSB.println("Begin");
   spi1.begin(SPI_QUARTER_SPEED, MSBFIRST, 0);
@@ -21,13 +24,17 @@ void setup() {
 
   SerialUSB.println("block(0):");
   sdcard.readBlock(0, blockBuffer);
-  for(int i=0; i<512; ) {
-    for(int col=0; col<16; col++, i++) {
+  for (int i = 0; i < 512;) {
+    for (int col = 0; col < 16; col++, i++) {
       SerialUSB.print(blockBuffer[i], 16);
       SerialUSB.print(" ");
     }
     SerialUSB.println();
   }
+
+  SerialUSB.print("cardSize:");
+  cardSize = sdcard.getCardSize();
+  SerialUSB.println(cardSize);
 
   pinMode(BOARD_LED_PIN, OUTPUT);
 }
--- firmware/usbMassStorageTest/main.cpp
+++ firmware/usbMassStorageTest/main.cpp
@@ -13,16 +13,28 @@ HardwareSPI spi1(SDCARD_SPI);
 Sd2Card sdcard(spi1, SDCARD_CS);
 
 void setup() {
+  delay(1000);
+  Serial1.begin(9600);
+  Serial1.println("begin");
+  pinMode(BOARD_LED_PIN, OUTPUT);
   spi1.begin(SPI_QUARTER_SPEED, MSBFIRST, 0);
   sdcard.begin();
-  USBMassStorage.begin();
 
-  pinMode(BOARD_LED_PIN, OUTPUT);
+  Serial1.println("getCardSize");
+  MAL_massBlockCount[0] = sdcard.getCardSize();
+  MAL_massBlockCount[1] = 0;
+  MAL_massBlockSize[0] = 512;
+  MAL_massBlockSize[1] = 0;
+  Serial1.print("cardSize = ");
+  Serial1.println((uint32_t) MAL_massBlockCount[0] * 512);
+
+  USBMassStorage.begin();
 }
 
 void loop() {
-  togglePin(BOARD_LED_PIN);
-  delay(1000);
+  USBMassStorage.loop();
+  //togglePin(BOARD_LED_PIN);
+  //delay(1000);
 }
 
 // Force init to be called *first*, i.e. before static object allocation.
@@ -43,10 +55,6 @@ int main(void) {
 }
 
 extern "C" uint16_t usb_mass_mal_init(uint8_t lun) {
-  MAL_massBlockCount[0] = sdcard.getCardSize();
-  MAL_massBlockCount[1] = 0;
-  MAL_massBlockSize[0] = 512;
-  MAL_massBlockSize[1] = 0;
   return 0;
 }
 
@@ -55,6 +63,12 @@ extern "C" uint16_t usb_mass_mal_get_status(uint8_t lun) {
 }
 
 extern "C" uint16_t usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *writebuff, uint16_t transferLength) {
+  //  Serial1.print("usb_mass_mal_write_memory(");
+  //  Serial1.print(memoryOffset);
+  //  Serial1.print(",");
+  //  Serial1.print(transferLength);
+  //  Serial1.println(")");
+
   if (lun != 0) {
     return USB_MASS_MAL_FAIL;
   }
@@ -65,16 +79,19 @@ extern "C" uint16_t usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset
 }
 
 extern "C" uint16_t usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) {
-  sdcard.readBlock(memoryOffset / 512, readbuff);
-  return USB_MASS_MAL_SUCCESS;
+  //  Serial1.print("usb_mass_mal_read_memory(");
+  //  Serial1.print(memoryOffset);
+  //  Serial1.print(",");
+  //  Serial1.print(transferLength);
+  //  Serial1.println(")");
 
-  //  if (lun != 0) {
-  //    return USB_MASS_MAL_FAIL;
-  //  }
-  //  if (sdcard.readBlock(memoryOffset / 512, readbuff)) {
-  //    return USB_MASS_MAL_SUCCESS;
-  //  }
-  //  return USB_MASS_MAL_FAIL;
+  if (lun != 0) {
+    return USB_MASS_MAL_FAIL;
+  }
+  if (sdcard.readBlock(memoryOffset / 512, readbuff)) {
+    return USB_MASS_MAL_SUCCESS;
+  }
+  return USB_MASS_MAL_FAIL;
 }
 
 extern "C" void usb_mass_mal_format() {
